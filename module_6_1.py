b1b2c0df3386fac62d293e245438f6ad976d0651
Gerich56 <gerichrus@mail.ru>
2024-11-02 00:51:48 +0500

    Update module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,26 +1,22 @@
-class Animal:
-    alive = True
-    fed = False
+class Animal():
+
     def __init__(self, name):
         self.name = name
+        self.alive = True
+        self.fed = False
 
     def eat(self, food):
-        if isinstance(food, Plant):
-            if food.edible:
-                print(f"{self.name} съел {food.name}")
-                self.fed = True
-            else:
-                print(f"{self.name} не стал есть {food.name}")
-                self.alive = False
+        if food.edible:
+            print(f'{self.name}, съел {food.name}')
+            self.fed = True
         else:
-            print(f"{self.name} не может есть {food}")
+            print(f'{self.name}, не стал есть {food.name}')
+            self.alive = False
+class Plant():
 
-
-class Plant:
-    edible = False
     def __init__(self, name):
         self.name = name
-
+        self.edible = False
 
 class Mammal(Animal):
     pass
